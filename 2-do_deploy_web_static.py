29d503634a97def1c5a37d9c9d7ebcaf6f837a40
Davies Brown <nwosudavid77.com>
2023-07-26 15:17:36 +0100

    fabric-deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ Fabric script that distributes an archive to your web servers
 
 from datetime import datetime
 from fabric.api import *
-import os
+from os.path import exists
 
 env.hosts = ["100.26.9.188", "100.26.227.244"]
 env.user = "ubuntu"
@@ -31,22 +31,16 @@ def do_deploy(archive_path):
     """
         Distribute archive.
     """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+    if exists(archive_path):
+        put(archive_path, '/tmp/')
 
-        print("New version deployed!")
+        filename = basename(archive_path)
+        file_ext = splitext(filename)[0]
+
+        sudo(f'mkdir -p /data/web_static/releases/{file_ext}')
+        sudo(f'tar -xzf /tmp/{filename} -C /data/web_static/releases/{file_ext}')
+        sudo('rm -rf /tmp/{filename}')
+        sudo('rm -rf /data/web_static/current')
+        sudo(f'ln -s /data/web_static/releases/{file_ext} /data/web_static/current')
         return True
-
     return False
